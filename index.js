4d127b416af79ea38d8c667d7e810f899b094dc8
deeryrain712 <85872444+deeryrain712@users.noreply.github.com>
2021-06-14 20:26:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 <b:if cond='data:blog.pageType in {&quot;static_page&quot;,&quot;item&quot;}'>
 <script type='text/javascript' src='https://cdn.jsdelivr.net/gh/deeryrain712/Source@main/index%20source.js'></script>
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js' type='text/javascript'></script>
 </b:if>
