f6455bc5ed06830e2efbec8917ef5e612f2dead0
deeryrain712 <85872444+deeryrain712@users.noreply.github.com>
2021-06-14 19:10:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+<b:if cond='data:blog.pageType in {&quot;static_page&quot;,&quot;item&quot;}'>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/deeryrain712/Source@main/index%20source.js'></script>
+</b:if>
