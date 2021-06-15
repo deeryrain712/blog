d068516ebffe3517e4a8eb6ba93fa592abd95c20
deeryrain712 <85872444+deeryrain712@users.noreply.github.com>
2021-06-15 08:14:36 +0700

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-<b:if cond='data:blog.pageType in {&quot;static_page&quot;,&quot;item&quot;}'>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/deeryrain712/Source@main/index%20source.js'></script>
-</b:if>
